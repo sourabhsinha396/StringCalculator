66a2dab023ed24207711958b89815fa6da90d446
Sourabh <sourabhsinha396@gmail.com>
2022-04-06 17:35:23 +0530

    refactor: group tests with similar responsibility in a class

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,27 +1,28 @@
 from main import StringCalculator
 
-def test_empty_string_should_return_zero():
-    string_calculator = StringCalculator("")
-    assert string_calculator.get_sum() == 0
+class TestStringCalculator:
+    def test_empty_string_should_return_zero(self):
+        string_calculator = StringCalculator("")
+        assert string_calculator.get_sum() == 0
 
 
-def test_one_value_should_return_itself():
-    string_calculator = StringCalculator("3")
-    assert string_calculator.get_sum() == 3
+    def test_one_value_should_return_itself(self):
+        string_calculator = StringCalculator("3")
+        assert string_calculator.get_sum() == 3
 
-    string_calculator = StringCalculator("4,")
-    assert string_calculator.get_sum() == 4
+        string_calculator = StringCalculator("4,")
+        assert string_calculator.get_sum() == 4
 
 
-def test_two_integers_should_add_correctly():
-    string_calculator = StringCalculator("3,5")
-    assert string_calculator.get_sum() == 8
+    def test_two_integers_should_add_correctly(self):
+        string_calculator = StringCalculator("3,5")
+        assert string_calculator.get_sum() == 8
 
-    string_calculator = StringCalculator("0,0")
-    assert string_calculator.get_sum() == 0
+        string_calculator = StringCalculator("0,0")
+        assert string_calculator.get_sum() == 0
 
-    string_calculator = StringCalculator("-2,-3")
-    assert string_calculator.get_sum() == -5
+        string_calculator = StringCalculator("-2,-3")
+        assert string_calculator.get_sum() == -5
 
 
 
