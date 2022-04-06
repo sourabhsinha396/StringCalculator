a6e3267659ddb7eebd14c6380abd195742feff30
Sourabh <sourabhsinha396@gmail.com>
2022-04-06 21:28:58 +0530

    accept new line delimeter

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from typing import List
 
 class StringCalculator:
     def __init__(self,values:str):
-        self.values = self._extract_numbers(values)
+        self.delimeter = "\n"
+        delimeter_processed = self._process_delimeter(values)
+        self.values = self._extract_numbers(delimeter_processed)
+
+    def _process_delimeter(self,values:str)->str:
+        delimeter_processed = values.replace("\n",",")
+        return delimeter_processed
 
     def _extract_numbers(self,values:str)->List:
         if values=="":
