f04caec71638f7d96f3f8cad498aaadd01d9e069
Sourabh <sourabhsinha396@gmail.com>
2022-04-06 17:30:41 +0530

    support for adding upto 2 numbers

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+from typing import Union,Tuple
+
+
+str_or_int = Union[str,int]
+
 class StringCalculator:
     def __init__(self,value:str):
-        self.first = self._extract_number(value)
+        self.first,self.second = self._extract_numbers(value)
 
-    def _extract_number(self,value:str)->int:
+    def _extract_numbers(self,value:str)->Tuple[str_or_int,str_or_int]:
         if value=="":
-            return  0
-        return value
+            return  0,0
+
+        if len(value.rstrip(","))==1:
+            return value.rstrip(","),0
+
+        values = value.split(",")
+        return values[0],values[1]
         
     def get_sum(self)->int:
-        return int(self.first)
+        return int(self.first) + int(self.second)
 
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -8,3 +8,20 @@ def test_empty_string_should_return_zero():
 def test_one_value_should_return_itself():
     string_calculator = StringCalculator("3")
     assert string_calculator.get_sum() == 3
+
+    string_calculator = StringCalculator("4,")
+    assert string_calculator.get_sum() == 4
+
+
+def test_two_integers_should_add_correctly():
+    string_calculator = StringCalculator("3,5")
+    assert string_calculator.get_sum() == 8
+
+    string_calculator = StringCalculator("0,0")
+    assert string_calculator.get_sum() == 0
+
+    string_calculator = StringCalculator("-2,-3")
+    assert string_calculator.get_sum() == -5
+
+
+
